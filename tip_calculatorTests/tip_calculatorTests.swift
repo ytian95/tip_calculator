e44d4f66542cd65779b1e4456416877f1e2d58ac
osxadmin <etsloaner@loanmac-005608.local>
2015-12-31 16:20:46 -0500

    Initial Commit

---
+++ tip_calculatorTests/tip_calculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  tip_calculatorTests.swift
+//  tip_calculatorTests
+//
+//  Created by etsloaner on 12/31/15.
+//  Copyright © 2015 ytian. All rights reserved.
+//
+
+import XCTest
+@testable import tip_calculator
+
+class tip_calculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
